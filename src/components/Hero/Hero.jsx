48d8baa8812c1e0515cde37139aae2ab6368bc69
Programmer-Adithya-Iyer <work.adithyaiyer@gmail.com>
2025-07-16 12:52:37 +0000

    Hero section done
    Image and main call to action added.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Hero.css";
 
 const Hero = () => {
-	return <div></div>;
+	return (
+		<div>
+			<main>
+				<h1>Not sure where to go? Perfect.</h1>
+				<span>I'm flexible</span>
+			</main>
+		</div>
+	);
 };
 
 export default Hero;
